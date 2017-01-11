0179021eac22dcf1bec0ce845ab2559c9ff844cb
tangrong <rong.tang@yoho.cn>
2017-01-11 17:45:46 +0800

    init project

---
+++ application/views/welcome_message.php
@@ -0,0 +1,10 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>static</title>
+  </head>
+  <body>
+    <div id="app"></div>
+    <script src="/static/dist/build.js"></script>
+  </body>
+</html>
